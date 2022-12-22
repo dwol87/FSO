1535384ddc9eb18c1ad817d833dcceb930195429
dwol87 <dwolinsky87@gmail.com>
2022-12-21 21:57:46 -0500

    changes again

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+  const total = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / total;
+  const positive = good * (100 / total);
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={handleGoodClick} text="Good" />
+      <Button handleClick={handleNeutralClick} text="Neutral" />
+      <Button handleClick={handleBadClick} text="Bad" />
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
+    </div>
+  );
+};
+
+export default App;
