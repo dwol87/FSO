4ba99326ade9829738ae4234c574ea6e6ccbd989
dwol87 <dwolinsky87@gmail.com>
2022-12-21 22:57:41 -0500

    found bug - no curly for component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,36 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  return total > 0 ? (
+    <table>
+      <tbody>
+        <Statistic text="Good" value={good} />
+        <Statistic text="Neutral" value={neutral} />
+        <Statistic text="Bad" value={bad} />
+        <Statistic text="Total" value={total} />
+        <Statistic text="Average" value={average} />
+        <Statistic text="Positive" value={positive} />
+      </tbody>
+    </table>
+  ) : (
+    <p>No feedback given</p>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
   const total = good + neutral + bad;
   const average = (good * 1 + bad * -1) / total;
   const positive = good * (100 / total);
@@ -22,16 +43,21 @@ const App = () => {
   return (
     <div>
       <Display name="Give Feedback" />
-      <Button handleClick={handleGoodClick} text="Good" />
-      <Button handleClick={handleNeutralClick} text="Neutral" />
-      <Button handleClick={handleBadClick} text="Bad" />
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}</p>
+
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+
+      <Display name="Statistics" />
+
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   );
 };
